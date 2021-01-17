4d0848e005d24d4123fc5a9c0f71b2fa410408d2
sis3141 <38073865+sis3141@users.noreply.github.com>
2021-01-17 17:00:06 +0900

    (JS) asset added

---
+++ Assets/Scripts/Trash/Csvtest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Csvtest : MonoBehaviour
+{
+    void Start()
+    {
+        List<Dictionary<string,object>> data = CSVReader.Read ("Data/ItemData");
+        Debug.Log("data count :"+data.Count);
+        for(var i=0; i<data.Count; i++)
+        {
+            Debug.Log(data[i]["Index"]+ " " + "Name: "+ data[i]["Name"] + " " + "desc: " + data[i]["Description"]);
+        }
+    }
+
+    
+}
